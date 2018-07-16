b3527d6a2f69646f53245a68b2b7c29b7b977988
Miraj <mirajmahmood@gmail.com>
2018-07-16 17:21:19 +0300

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+function initialise(errors, kwtData, ordersJson, costJson){
+
+	get_vendors();
+	get_customers(kwtData, ordersJson, costJson);
+	get_drivers();
+	
+}
+
+
+d3.queue()
+    .defer(d3.json, "https://query.data.world/s/dk5r7fnrj4fmvstrw2abu66sp3vtym") //kwt geospatial file
+    .defer(d3.json, "https://query.data.world/s/e6tdhwkamycjnwev6mtjhvyug6om5n") //march orders
+    .defer(d3.json, "https://query.data.world/s/csf6y4ljcvzy65euzk6wuocbl53foo") //march cost
+    .await(initialise);
